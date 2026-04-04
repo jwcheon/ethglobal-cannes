c20902598832282d06e21e88e8540f40f60e7422
Jinwoo Cheon <jinwoo@sosleek.io>
2026-04-04 21:02:04 +0200

    add API key injection to WC proxy

--- pos-web/functions/wcpay/[[path]].ts
+++ pos-web/functions/wcpay/[[path]].ts
@@ -1,5 +1,9 @@
 // Proxy to WalletConnect Pay API
-export const onRequest: PagesFunction = async (context) => {
+interface Env {
+  WC_API_KEY: string;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
   const url = new URL(context.request.url);
   const path = url.pathname.replace('/wcpay', '');
 
@@ -8,6 +12,11 @@ export const onRequest: PagesFunction = async (context) => {
   // Clone the request with the new URL
   const headers = new Headers(context.request.headers);
   headers.delete('host');
+
+  // Inject API key if not present (for gateway calls from customer wallet)
+  if (!headers.has('Api-Key') && context.env.WC_API_KEY) {
+    headers.set('Api-Key', context.env.WC_API_KEY);
+  }
 
   const response = await fetch(targetUrl, {
     method: context.request.method,
@@ -21,7 +30,7 @@ export const onRequest: PagesFunction = async (context) => {
   const responseHeaders = new Headers(response.headers);
   responseHeaders.set('Access-Control-Allow-Origin', '*');
   responseHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  responseHeaders.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  responseHeaders.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Api-Key, Merchant-Id');
 
   return new Response(response.body, {
     status: response.status,
